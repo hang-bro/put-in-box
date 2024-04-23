9dfc4edf85a34066817bcfeb43fa6d6137fc9b7e
huangyuhang <772269729@qq.com>
2024-04-23 13:33:45 +0800

    use vite create react project

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    host: 'localhost',
-    port: 8080,
-    open: true,
-    proxy: {
-      '/api': {
-        target: '后端接口域名',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
